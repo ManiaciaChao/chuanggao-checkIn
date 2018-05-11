27b9d9715032550322136756bcf44dcb24b91509
letra <letra@bingyan.net>
2018-05-11 23:31:19 +0800

    add request

---
+++ core/createRequest.js
@@ -0,0 +1,26 @@
+const request = require('request');
+const querystring = require('querystring');
+
+function createRequest(url, method, data, callback) {
+  const options = {
+    url, 
+    method, 
+    headers: {
+      'User-Agent': 'ChingoItemCGTY(Linux; iOS 11.2;iPhone HUUID/B6E3A84E-B928-4720-A6F9-A1390B3ED6BD)',
+      'Accept': '*/*',
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Connection': 'keep-alive',
+      'Accept-Encoding': 'br, gzip, deflate',
+    },
+  };
+
+  if (method.toUpperCase() === 'POST') {
+    options.form = data;
+  } else if (method.toUpperCase() === 'GET') {
+    options.url += `?${querystring.stringify(data)}`;
+  }
+
+  request(options, callback);
+}
+
+module.exports = createRequest;
--- core/index.js
+++ core/index.js
@@ -1,10 +1,27 @@
 const getSign = require('../utils/getSign');
 const getJsonSports = require('../utils/getJsonSports');
+const createRequest = require('./createRequest');
 
 const app_key = 'cgsoft';
+const baseUrl = 'http://petzjk.hust.edu.cn/cgapp-server/';
 const apiString = '/api/v3/savesports';
 const timestamp = +new Date();
 const jsonsports = JSON.stringify(getJsonSports());
 const sign = getSign({jsonsports}, apiString, timestamp);
 
-console.log({app_key, jsonsports, sign, timestamp});
+
+const data = {
+  app_key, 
+  jsonsports, 
+  sign, 
+  timestamp
+}
+
+createRequest(`${baseUrl}${apiString}`, 'POST', data, (error, response) => {
+  if(error) {
+    throw error;
+  }
+  if(!error && response.statusCode === 200) {
+    console.log(response.body);
+  }
+});
